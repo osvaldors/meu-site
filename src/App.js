10513a36b628400ed24dc1b2b519f2c5f4648f8e
osvaldors <osvaldorodrigues89@gmail.com>
2025-04-11 16:30:13 -0300

    1comit React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Hello World!</h1>
+            <p>Meu primeiro aplicativo React</p>
+        </div>
+    );
+}
+
+export default App;
