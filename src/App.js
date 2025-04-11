bb5ca6d1173e6a9712a6972735a16205b5414f09
osvaldors <osvaldorodrigues89@gmail.com>
2025-04-11 17:09:58 -0300

    a

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="app">
             <h1>Hello World!</h1>
-            <p>Meu primeiro aplicativo React sss</p>
+            <p>---</p>
         </div>
     );
 }
